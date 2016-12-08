058b8e72f53181e0a5fe53e42cf1d7e7162c5f67
160096 <yse160096@gmail.com>
2016-12-08 11:50:22 +0900

    ５回目のコミットです。

---
+++ 20161208/CopyByteText.java
@@ -0,0 +1,25 @@
+import java.io.*;
+public class CopyByteText{
+	public static void main(String[]args){
+FileInputStream inFile = null;
+FileOutputStream outFile = null;
+
+try{
+	inFile = new FileInputStream("TXXXT.txt");
+	outFile = new FileOutputStream("resulttxxt.txt");
+	System.out.println("Streamを作りました。");
+	//int data = inFile.read();
+	int data;
+	while((data = inFile.read())!=-1){
+	outFile.write((char)data);
+	System.out.println("テキストファイルをコピーしています。");
+	}
+	inFile.close();
+	System.out.println("コピー終わりました。");
+	outFile.close();
+}catch(IOException e){
+	System.err.println("エラーです");
+	System.exit(1);
+		}
+}
+}
