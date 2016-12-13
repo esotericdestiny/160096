7e02e7a29564f6bd2adc6e782500bb87f86a9f0e
160096 <yse160096@gmail.com>
2016-12-13 09:28:56 +0900

    ４回に１回アロハ

--- 20161213/Aloha.java
+++ 20161213/Aloha.java
@@ -2,7 +2,11 @@ public class Aloha{
 	public static void main(String[]args){
 		int i =0;
 		while(i<20){
+			if(i%4==0){
+				System.out.println("アロハ");
+			}else{
 		System.out.println("Aloha");
+		}
 		i++;
 		}
 	}
