d215c28102bc0e28ad1f401e2610fa99133a336b
160096 <yse160096@gmail.com>
2016-12-13 09:43:20 +0900

    ４回目のCOMMIT

--- 20161213/Aloha.java
+++ 20161213/Aloha.java
@@ -1,9 +1,13 @@
 public class Aloha{
 	public static void main(String[]args){
-		int i =0;
-		while(i<20){
-				System.out.println(args[0]);
-		i++;
+		String data;
+		while (true){
+			System.out.print("Input data >");
+			data = new java.util.Scanner(System.in).nextLine();
+			System.out.println(data);
+			if(data.equals("exit")){
+				break;
+			}
 		}
 	}
 }
