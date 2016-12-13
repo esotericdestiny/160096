fa55668989de7f90446bda674f519a32e237f36e
160096 <yse160096@gmail.com>
2016-12-13 09:31:32 +0900

    Args20回表示する

--- 20161213/Aloha.java
+++ 20161213/Aloha.java
@@ -2,11 +2,7 @@ public class Aloha{
 	public static void main(String[]args){
 		int i =0;
 		while(i<20){
-			if(i%4==0){
-				System.out.println("アロハ");
-			}else{
-		System.out.println("Aloha");
-		}
+				System.out.println(args[0]);
 		i++;
 		}
 	}
