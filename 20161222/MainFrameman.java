eb99a0e029e24fba876f4f06d063ccf1753b1145
160096 <yse160096@gmail.com>
2016-12-22 09:56:51 +0900

    FrameMan added.

---
+++ 20161222/MainFrameman.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import javax.swing.*;
+public class MainFrameman{
+public static void main(String[] args) {
+	FrameMan fm = new FrameMan();
+
+}
+}
+class FrameMan{
+	JFrame frame;
+	public FrameMan(){
+		System.out.println("Aloha2222222");
+	}
+}
