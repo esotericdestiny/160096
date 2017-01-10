c7652be33275ecaf29d21b266b34bcabbf57320a
160096 <yse160096@gmail.com>
2017-01-10 11:56:09 +0900

    Aloha changed.

--- 20170110/Aloha.java
+++ 20170110/Aloha.java
@@ -1,7 +1,9 @@
 public class Aloha{
 	public static void main(String[] args) {
-		Alohaman am = new Alohaman();
-		am.sayAloha(319,5);
+		Alohaman am = new Alohaman(args[0], args[1]);
+		int data1 = Integer.parseInt(args[2]);
+		int data2 = Integer.parseInt(args[3]);
+		am.sayAloha(data1,data2);
 }
 }
 class Alohaman{
@@ -11,6 +13,11 @@ class Alohaman{
 	this.msg = "Aloha";
 	this.msg2 = "青木！";
 }
+	public Alohaman(String m1, String m2){
+	this.msg = m1;
+	this.msg2 = m2;
+}
+
 	public void sayAloha(){
 		System.out.println(this.msg);
 }
